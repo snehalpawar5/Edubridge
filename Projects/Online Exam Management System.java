64f2d5a09553fffd8e704cc7dbfb4fc70fd240ee
Snehal <86770853+snehalpawar5@users.noreply.github.com>
2021-10-29 02:31:27 +0530

    Update Online Exam Management System.java

--- Projects/Online Exam Management System.java
+++ Projects/Online Exam Management System.java
@@ -1,3 +1,9 @@
+/*In the existing system, all works are done manually. There are chances of committing errors and it will take more time to perform any transaction.
+There are so many limitations in the existing system.The proposed system overcomes all the drawbacks of the existing examination system.
+Being an online application it can be accessed from anywhere. It is fast, efficient and economical.*/
+
+
+
 import java.io.*;
 //import java.lang.*;
 
