2465d807c9dec0cdf1a4a8c6d92b00b964278598
Snehal <86770853+snehalpawar5@users.noreply.github.com>
2021-10-28 22:22:12 +0530

    Update GameOfThrown.java

--- Projects/GameOfThrown.java
+++ Projects/GameOfThrown.java
@@ -1,3 +1,12 @@
+/* Rock-Paper-Scissors:
+It is a two player game and contains three main components Rock, Paper and Scissors.
+Each player has these components and simultaneously chooses either Rock, Paper, or Scissors. 
+It has some rules like:
+1.Rock beats Scissors but loses to Paper.
+2.Paper beats Rock but loses to Scissors.
+3.Scissors beats Paper but loses to Rock.*/
+	
+	
 import java.util.Scanner;
 import java.util.Random;
 public class GameOfThrown 
