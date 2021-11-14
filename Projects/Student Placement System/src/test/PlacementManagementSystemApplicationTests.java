11e5f964355ad38bfda626012069465cbf777a4a
Snehal <86770853+snehalpawar5@users.noreply.github.com>
2021-11-14 20:08:10 +0530

    Add files via upload

---
+++ Projects/Student Placement System/src/test/PlacementManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.PlacementManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
